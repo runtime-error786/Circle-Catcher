162f8d3f1925125a4f1327769a9418aee35cd356
runtime-error786 <mustafa782a@gmail.com>
2024-07-24 13:07:10 +0500

    generate circle

--- game.py
+++ game.py
@@ -8,3 +8,9 @@ mp_hands = mp.solutions.hands
 hands = mp_hands.Hands(max_num_hands=1, min_detection_confidence=0.7, min_tracking_confidence=0.7)
 mp_drawing = mp.solutions.drawing_utils
 
+def generate_circle(frame_height, frame_width):
+    x = random.randint(50, frame_width - 50)
+    y = random.randint(50, frame_height - 50)
+    radius = 30
+    return (x, y, radius)
+
