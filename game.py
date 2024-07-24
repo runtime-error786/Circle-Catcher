d95ecc9b906019cff73ea245f4c7669cda45eb7c
runtime-error786 <mustafa782a@gmail.com>
2024-07-24 13:05:51 +0500

    media pipeline setup

---
+++ game.py
@@ -0,0 +1,10 @@
+import cv2
+import numpy as np
+import random
+import time
+import mediapipe as mp
+
+mp_hands = mp.solutions.hands
+hands = mp_hands.Hands(max_num_hands=1, min_detection_confidence=0.7, min_tracking_confidence=0.7)
+mp_drawing = mp.solutions.drawing_utils
+
